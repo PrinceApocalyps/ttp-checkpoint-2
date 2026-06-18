3d5018d79252619ca327a63a4135dee76afdf174
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-06-18 09:39:37 -0400

    commit: Part 3 Lists

--- src/Part3_Lists.jsx
+++ src/Part3_Lists.jsx
@@ -55,10 +55,22 @@ function SectionA() {
       <h3>All Players</h3>
       <ul>
         {/* A1: map players here: */}
+        {players.map((player)=>{
+          return <li key={player.id}>Name: {player.name}, score: {player.score}</li>
+        })}
 
       </ul>
 
       {/* A2: filtered list goes here: */}
+      <h3>Score above 30</h3>
+      {
+        players.filter((player)=>{
+          return player.score>30
+        })
+        .map((player)=>{
+          return <li key={player.id}>Name: {player.name}, score: {player.score}</li>
+        })
+      }
 
     </div>
   )
@@ -80,7 +92,15 @@ function SectionA() {
 //
 // Write PlayerRow here:
 
-
+function PlayerRow(props){
+  return(
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Score: {props.score}</p>
+      <br />
+    </div>
+  )
+}
 
 function SectionB() {
   // B2.
@@ -100,7 +120,11 @@ function SectionB() {
     <div>
       <h2>Section B — Lists and Components</h2>
       {/* B2: map PlayerRow components here */}
-
+      {
+        players.map((player)=>{
+          return <PlayerRow key={player.id} name={player.name} score={player.score}></PlayerRow>
+        })
+      }
     </div>
   )
 }
